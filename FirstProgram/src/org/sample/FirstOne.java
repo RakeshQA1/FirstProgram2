eaa7bb507f672198a392fa74e6ada04132d1e4e1
Rakesh <Rakesh@DESKTOP-MLJO9SN>
2024-07-30 11:18:55 +0530

    First Commit

---
+++ FirstProgram/src/org/sample/FirstOne.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class FirstOne {
+	
+	private void sam() {
+		System.out.println("bye bye");
+
+	}
+	
+	 public static void main(String[] args) {
+		 
+		 FirstOne f = new FirstOne();
+		 f.sam();
+		
+	}
+
+}
